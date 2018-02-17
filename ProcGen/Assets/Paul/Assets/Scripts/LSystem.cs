fc6615303348709019b1986665ed3fb6b7b07f7b
PGrad <paulgrad3rd@gmail.com>
2018-02-16 21:35:53 -0800

    Hello, it's Paul! Here is a working draft of my lsystem generator and renderer for mazes, fractals, etc.

---
+++ ProcGen/Assets/Paul/Assets/Scripts/LSystem.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Text;
+
+public class LSystem : MonoBehaviour {
+
+	public Rules rules;
+	public string sentence;
+	public string sentenceName;
+
+	public void Generate() {
+		StringBuilder nextAxiom = new StringBuilder();
+		string production;
+		foreach(char c in sentence) {
+			production = c.ToString();
+			foreach(Rule rule in rules.rules)
+				if(rule.LHS == c) {
+					production = rule.RHS;
+					break;
+				}
+			nextAxiom.Append(production);
+		}
+		sentence = nextAxiom.ToString();
+	}
+}
---
+++ ProcGen/Assets/Paul/Assets/Scripts/Rules.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class Rules : ScriptableObject {
+    public List<Rule> rules;
+    [MenuItem("Assets/Create/LRules")]
+    public static void create() {
+        Rules asset = Rules.CreateInstance<Rules>();
+
+        AssetDatabase.CreateAsset(asset, "Assets/NewRules.asset");
+        AssetDatabase.SaveAssets();
+
+        EditorUtility.FocusProjectWindow();
+
+        Selection.activeObject = asset;
+    }
+}
